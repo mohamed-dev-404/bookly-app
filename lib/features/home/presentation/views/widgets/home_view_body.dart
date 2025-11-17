af5ee3a1390fe2f128b8c133853e22adad312814
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2025-11-17 16:19:52 +0200

    feat: add BestSellerListViewItem widget with book cover, title, author,price, and rating.

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,5 @@
 import 'package:bookly/core/utils/app_styles.dart';
-import 'package:bookly/features/home/presentation/views/widgets/best_seller_book_image.dart';
+import 'package:bookly/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -24,23 +24,6 @@ class HomeViewBody extends StatelessWidget {
           SizedBox(height: 25),
           BestSellerListViewItem(),
         ],
-      ),
-    );
-  }
-}
-
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return const SizedBox(
-      child: SizedBox(
-        height: 100,
-        child: Row(
-          children: [
-            BestSellerBookImage(),
-          ],
-        ),
       ),
     );
   }
