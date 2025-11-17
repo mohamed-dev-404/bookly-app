45806bc15b9a020ef1693e51797c2ffdb306acb3
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2025-11-18 00:29:57 +0200

    refactor: convert HomeViewBody structure from Column to CustomScrollView and integrate BestSellerListView into sliver layout, and complete the HomeView UI layout.

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,5 @@
 import 'package:bookly/core/utils/app_styles.dart';
-import 'package:bookly/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
+import 'package:bookly/features/home/presentation/views/widgets/best_seller_list_view.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -11,18 +11,25 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.only(left: 32.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppBar(),
-          FeaturedBooksListView(),
-          SizedBox(height: 50),
-          Text(
-            'Best Seller',
-            style: AppStyles.text18SemiBold,
+      child: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomAppBar(),
+                FeaturedBooksListView(),
+                SizedBox(height: 50),
+                Text(
+                  'Best Seller',
+                  style: AppStyles.text18SemiBold,
+                ),
+                SizedBox(height: 25),
+              ],
+            ),
           ),
-          SizedBox(height: 25),
-          BestSellerListViewItem(),
+          BestSellerListView(),
         ],
       ),
     );
