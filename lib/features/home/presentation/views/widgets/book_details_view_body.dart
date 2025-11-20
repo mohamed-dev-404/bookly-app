01896de23848d7bb34eccc9256a6c1dfe7efa14f
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2025-11-20 21:43:58 +0200

    feat: create custom BookDetailsAppBar widget.

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -7,6 +8,15 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const SafeArea(
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: CustomBookDetailsAppBar(),
+          ),
+        ],
+      ),
+    );
   }
 }
