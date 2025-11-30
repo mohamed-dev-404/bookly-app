062fb2cdbbde0215de95d277ab6cd985d14798bd
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2025-11-30 19:33:55 +0200

    feat: implement  SimilarBooksListView with horizontal scroll of book covers.

---
+++ lib/features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -0,0 +1,27 @@
+import 'package:bookly/features/home/presentation/views/widgets/custom_book_cover.dart';
+import 'package:flutter/material.dart';
+
+class SimilarBooksListView extends StatelessWidget {
+  const SimilarBooksListView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final hight = MediaQuery.of(context).size.height;
+    return SizedBox(
+      height: hight * .17,
+      child: ListView.separated(
+        physics: const BouncingScrollPhysics(),
+        itemCount: 15,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return const CustomBookCover();
+        },
+        separatorBuilder: (context, index) {
+          return const SizedBox(width: 10);
+        },
+      ),
+    );
+  }
+}
