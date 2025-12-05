8851cb2ba3cbf2f0affdef25544ce0fba3f0e48b
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2025-12-05 19:37:34 +0200

    feat: define API endpoints constants.

---
+++ lib/core/network/constant/api_endpoints.dart
@@ -0,0 +1,16 @@
+class EndPoints {
+  //? Base URL
+  static String baseUrl = 'https://www.googleapis.com/books/v1/';
+
+  //? EndPoints
+  static String volumes = 'volumes';
+  // static String logIn = 'auth/login';
+  // static String register = 'auth/register';
+  // static String forgetpassword = 'auth/forgotten-password';
+  // static String virfyOTP = 'auth/verify-token';
+  // static String resetPassword = 'auth/reset-password';
+  // static String addProduct = 'admin/products';
+  // static String uploadProductImageEndPoint(id) {
+  //   return 'admin/products/$id/image';
+  // }
+}
