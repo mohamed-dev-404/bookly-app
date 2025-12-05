37ee9903b96abd029af6f99ce7a8a493fe401ca6
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2025-12-05 19:49:43 +0200

    feat: add AppLogInterceptor for custom logging requests, responses, and errors.

---
+++ lib/core/network/interceptors/app_log_interceptor.dart
@@ -0,0 +1,33 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class AppLogInterceptor extends Interceptor {
+  final _log = debugPrint;
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    _log('============ REQUEST ============');
+    _log('➡️  ${options.method} ${options.path}');
+    _log('Headers: ${options.headers}');
+    _log('Query: ${options.queryParameters}');
+    _log('Body: ${options.data}');
+    super.onRequest(options, handler);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    _log('============ RESPONSE ============');
+    _log('➡️  ${response.statusCode} ${response.requestOptions.path}');
+    _log('Body: ${response.data}');
+    super.onResponse(response, handler);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    _log('============ ERROR ============');
+    _log('❌ ${err.type}');
+    _log('StatusCode: ${err.response?.statusCode}');
+    _log('Message: ${err.message}');
+    super.onError(err, handler);
+  }
+}
